7799df546384601f98c7ed30fe873416e196b694
Rajesh <rajeshelluru143@gmail.com>
2026-03-29 18:24:04 +0530

    Optimize fruit selection using Pandas DataFrame
    Refactor code to use Pandas DataFrame for fruit options and remove unnecessary loops.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,13 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-fruit_rows = my_dataframe.collect()
 
-fruit_name_to_search = {}
-fruit_display_names = []
-
-for row in fruit_rows:
-    fruit_display_names.append(row["FRUIT_NAME"])
-    fruit_name_to_search[row["FRUIT_NAME"]] = row["SEARCH_ON"]
+# Convert Snowpark DataFrame to Pandas DataFrame
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    fruit_display_names,
+    pd_df['FRUIT_NAME'].tolist(),
     max_selections=5
 )
 
@@ -38,11 +33,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         try:
-            search_on = fruit_name_to_search[fruit_chosen]
-
             smoothiefroot_response = requests.get(
                 f"https://my.smoothiefroot.com/api/fruit/{search_on}"
             )
