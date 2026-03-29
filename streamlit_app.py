c7fb9895d08e06638f6c28e643d3fe5823be51b4
Rajesh <rajeshelluru143@gmail.com>
2026-03-29 18:16:11 +0530

    Enhance fruit selection and API integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,19 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+fruit_rows = my_dataframe.collect()
+
+fruit_name_to_search = {}
+fruit_display_names = []
+
+for row in fruit_rows:
+    fruit_display_names.append(row["FRUIT_NAME"])
+    fruit_name_to_search[row["FRUIT_NAME"]] = row["SEARCH_ON"]
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe,
+    fruit_display_names,
     max_selections=5
 )
 
@@ -31,22 +38,25 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        smoothiefroot_response = requests.get(
-            f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen.lower().replace(' ', '_')}"
-        )
+        st.subheader(fruit_chosen + ' Nutrition Information')
 
-        sf_df = st.dataframe(
-            data=smoothiefroot_response.json(),
-            use_container_width=True
-        )
+        try:
+            search_on = fruit_name_to_search[fruit_chosen]
 
-    # st.write(ingredients_string)
+            smoothiefroot_response = requests.get(
+                f"https://my.smoothiefroot.com/api/fruit/{search_on}"
+            )
+
+            sf_df = st.dataframe(
+                data=smoothiefroot_response.json(),
+                use_container_width=True
+            )
+
+        except Exception as e:
+            st.write("Sorry, that fruit is not available in the API.")
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                         values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    # st.write(my_insert_stmt)
-    # st.stop()
 
     time_to_insert = st.button('Submit Order')
 
